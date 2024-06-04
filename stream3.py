4d94d587503a04930d35c8bf3f90a5543c4b5751
Darryl Williams <39777038+njasharp@users.noreply.github.com>
2024-06-04 11:49:36 -0700

    Add files via upload
    added data

---
+++ stream3.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config (layout="wide")
+
+#header - header of the app 
+st.header("using streamlit intput widgets ")
+
+
+reg_form = st.form("user_registration_form")
+first_name = reg_form.text_input('First Name: ', key='fname')
+last_name = reg_form.text_input('Last Name: ', key='Lname')
+password = reg_form.text_input('Password: ', type='password', key='password')
+age = reg_form.slider("What is your cuerrnt age?")
+level = reg_form.radio("What is your membership level?", ['Silver', 'Gold', 'Platinum'])
+status = reg_form.checkbox("Active Member?")
+submit_button = reg_form.form_submit_button('Submit')
+
+if submit_button:
+    st.success('Welcome' + ' ' + first_name + " " + last_name)
+
+
+df = pd.read_csv(r".\data1.csv")
+st.write(df)
+dc = {"a" : 10, "b" : 20, "c" : 30}
+st.write(dc)
