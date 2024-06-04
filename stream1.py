ed6f839d32dacb60e09d1bb87affc0983dbf5404
Darryl Williams <39777038+njasharp@users.noreply.github.com>
2024-06-04 10:37:49 -0700

    Add files via upload
    test deploy streamlit

---
+++ stream1.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.set_page_config (layout="wide")
+
+st.header("Past Incidents")
+st.subheader("Jun 4, 2024")
+st.write(":red[ChatGPT is unavailbe for some users]")
+st.write("**Resolved:** this has been resolved")
+st.text("jun 4 04:45 PST")
+st.write("**Monitoring:** this has been resolved")
+st.text("jun 4 04:45 PST")
+st.write("**Update:** this has been resolved")
+st.text("jun 4 04:45 PST")
+st.write("**Under reivew:** this has been resolved")
+st.text("jun 4 04:45 PST")
+
