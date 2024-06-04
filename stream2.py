8d984e30c50451a8e7fb5645c306fa7b6e35adf9
Darryl Williams <39777038+njasharp@users.noreply.github.com>
2024-06-04 11:41:33 -0700

    Add files via upload
    added new text

---
+++ stream2.py
@@ -0,0 +1,28 @@
+import streamlit as st
+st.set_page_config (layout="wide")
+
+#title - title of the app
+st.title("Welcome to pad")
+
+#header - header of the app 
+st.header("Machine learning")
+
+#sub header - 
+st.subheader("Deep learning")
+
+#information 
+st.info("information of the user")
+
+#warning
+st.warning("come on time ")
+
+
+#st.error
+st.error("error")
+
+
+#write text 
+st.write("staff name")
+st.write(range(50))
+
+
